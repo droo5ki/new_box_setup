e4ca1bd6bf8ff88edaca77053aa4f2fe96ffd726
droo5ki <etch.himself@gmail.com>
2016-07-20 22:46:44 -0700

    updates to switch_default_app_links, added ctags to brew install for tagbar vim plugin.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # new_box_setup
-scripts to ease into a new box, I'm tired of doing this all manually :-)
+script(s) to ease into a new box, because I'm tired of doing this all manually everytime. :-)
 
 # Installation:
 `sh -c (curl -sSLhttps://raw.githubusercontent.com/droo5ki/new_box_setup/master/install.sh)`
--- install.sh
+++ install.sh
@@ -27,15 +27,18 @@ check_for_brew_and_install(){ # there is an assumption here that the target syst
 install_brew_stuff(){
     
     echo "Installing..."
-    brew install git tmux python vim shellcheck reattach-to-user-namespace 
+    brew install git tmux python vim shellcheck reattach-to-user-namespace ctags
     brew cask install google-chrome spectacle iterm2 atext caffeine
 }
 
 switch_default_app_links(){
 
-    # this will be for making sure the brew versions of git, python, vim etc.
-    # are correctly being run
-	true;
+    # remove the old version of vim and symlink the brew version
+    # Note: on el capitan this won't work because of System Integrity Protection
+    # do this first in that case: http://stackoverflow.com/a/32661637
+
+    sudo rm /usr/bin/vim
+    sudo ln -s /usr/local/bin/vim /usr/bin/vim
 }
 
 setup_vim(){
@@ -51,6 +54,8 @@ setup_vim(){
     else 
         echo "You don't seem to have a .vim/colors directory."
     fi
+
+
 }
 
 get_dotfiles(){
@@ -58,8 +63,9 @@ get_dotfiles(){
     cd "$HOME" || exit
 	git clone https://github.com/droo5ki/dotfiles.git 
     mv dotfiles .dotfiles 
-    cd .dotfiles || exit
-    find "$(pwd) -name '.*rc' | cut -d '/' -f5 | xargs -I {} ln -snf $HOME/.dotfiles/{} $HOME/{}"
+    find $HOME/.dotfiles -name '.*rc' | cut -d '/' -f5 | xargs -I {} ln -snf $HOME/.dotfiles/{} $HOME/{}
+    ln -snf $HOME/.tmux.conf $HOME/.tmux.com
+
 }
 
 install_oh_my_zsh(){
@@ -70,7 +76,6 @@ install_oh_my_zsh(){
 
 main(){
 
-    #check_for_sudo 
 	check_for_brew_and_install
     install_oh_my_zsh
 	get_dotfiles
